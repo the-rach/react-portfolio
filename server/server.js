9bb8fa36d7f43e64764343a9b0d848c7f2ed4300
rachelgrant <msrachelgrant@gmail.com>
2023-08-23 14:07:48 -0600

    updated server to allow https with ssl cert in production

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+const fs = require('fs'); // Required for reading certificates
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
@@ -14,6 +16,28 @@ app.use(bodyParser.json())
 
 // Middleware that enables Cross-Origin Resource Sharing (CORS).
 app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+
+  // Load SSL/TLS certificates for production enviroment
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/rachelgrant.me/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/rachelgrant.me/fullchain.pem', 'utf8');
+  const chain = fs.readFileSync('/etc/letsencrypt/live/rachelgrant.me/chain.pem', 'utf8');
+
+  const credentials = {
+      key: privateKey,
+      cert: certificate,
+      ca: chain
+  };
+
+  // Create an HTTPS server for production
+  const httpsServer = https.createServer(credentials, app);
+  server = httpsServer;
+}
+else {
+    // Create an HTTP server for development
+    server = app;
+}
 
 /*
  * Route that responds with a simple "Hello World!" message.
@@ -34,10 +58,11 @@ app.get('/', (req, res) => {
 app.use('/api', educationRouter)
 
 /*
- * Start the API server and listen on the specified port.
- * @name listen
- * @function
- * @param {number} port - The port to listen on.
- * @param {Function} callback - The callback function to execute once the server is running.
- */
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+  * Start the API server and listen on the specified port.
+  * @name listen
+  * @function
+  * @param {number} port - The port to listen on.
+  * @param {Function} callback - The callback function to execute once the server is running.
+  */
+server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
